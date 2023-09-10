1f237b8791fc09cc3b6753a01ca888306d4a0d5c
Pritish2005 <pritishmahajan12@gmail.com>
2023-09-10 08:19:29 +0530

    Day1

--- Top 150/Linked_List_Cycle.cpp
+++ Top 150/Linked_List_Cycle.cpp
@@ -8,21 +8,21 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(fast!=NULL){
-            fast=fast->next;
-            if(fast!=NULL){
-                fast=fast->next;
-                slow=slow->next;
-                if(slow==fast){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-};
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         ListNode* slow=head;
+//         ListNode* fast=head;
+//         while(fast!=NULL){
+//             fast=fast->next;
+//             if(fast!=NULL){
+//                 fast=fast->next;
+//                 slow=slow->next;
+//                 if(slow==fast){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
